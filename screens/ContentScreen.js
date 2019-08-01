da7613f7264273cc299f79f605e260d71c157944
Sunghee <630sunghee@gmail.com>
2019-08-02 00:45:05 +0900

    Fix a bug in text color

--- screens/ContentScreen.js
+++ screens/ContentScreen.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { Button, Image, View ,TextInput,Text, StyleSheet, Alert} from 'react-native';
-// import {Constants} from 'expo-constants';
-// import { Camera } from 'expo-camera';
+import { Image, View, TextInput, StyleSheet, Alert } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
-
+import { Button, Text } from 'native-base';
+import { colors, fonts } from '../theme';
 
 export default class ContentScreen extends React.Component {
     constructor(props) {
@@ -26,15 +25,6 @@ export default class ContentScreen extends React.Component {
         await Permissions.askAsync(Permissions.CAMERA_ROLL);
     }
 
-    // _pickImage = async () => {
-    //     let pickerResult = await ImagePicker.launchImageLibraryAsync({
-    //             allowsEditing: true,
-    //             aspect: [3, 3.5],
-    //             });
-    
-    //     // this._handleImagePicked(pickerResult);
-    //   };
-
     _pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -47,34 +37,37 @@ export default class ContentScreen extends React.Component {
         }
     };
 
-    handleTextInput=text=>{                         //text
-        this.setState({textIn:text});
+    handleTextInput=(text)=>{                         //text
+        this.setState({text: text});
     };
-    Capsulize=(image,textIn)=>{                     //capsule
-        alert("Keep your memory");
-    };
+
     render() {
         let { image } = this.state;
 
         return (
-            <View style={{flex:1,alignItems:"center", justifyContent:"center"}}>
-                <Button
-                    title= "Select image"
-                    onPress={this._pickImage}
-                />
+            <View style={{flex: 1, alignItems:"center", justifyContent:"center"}}>
+                { image ? null : 
+                    <Button transparent
+                        onPress={this._pickImage}
+                    >
+                        <Text>Select image</Text>
+                    </Button>
+                }
                 <TextInput
-                    style={{height: 300,width:300, borderColor: 'gray', borderWidth: 1}}
+                    style={{height: 300,width:300, borderColor: 'black', borderWidth: 1}}
                     placeholder="Type here the contents"
-                    onChangeText={this.handleTextInput}
+                    onChangeText={ this.handleTextInput }
                 />
-                <Text style ={{padding:10,fontSize:42}}></Text>
                 { image &&
-                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-                <TouchableOpacity 
-                    style={StyleSheet.submitButton}
-                    onPress={()=>this.Capsulize(this.state.image,this.state.textIn)}>
-                    <Text style={StyleSheet.submitButtonText}>Keep the memory</Text>
-                </TouchableOpacity>
+                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+                }
+                <View>
+                    <Button large
+                        onPress={() => this.props.navigation.navigate('Main')}
+                        >
+                        <Text style={{color:'white'}}>Keep the memory!</Text>
+                    </Button>
+                </View>
             </View>
         );
     }
